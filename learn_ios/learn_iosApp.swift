ae114523e9f0b31f591136f2643585a9f8f6dd04
Abdul Azis Al Ayubbi <azisa6980@gmail.com>
2023-03-06 11:30:27 +0700

    Initial Commit

---
+++ learn_ios/learn_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  learn_iosApp.swift
+//  learn_ios
+//
+//  Created by Abdul Azis Al Ayubbi on 06/03/23.
+//
+
+import SwiftUI
+
+@main
+struct learn_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
